fa89f41fec79b1e4b0962565c5c4d291fdf3d98f
ironman-eric <eonno123@yahoo.com>
2023-09-17 17:07:01 -0700

    client & login

--- src/pubSub/subscribe.js
+++ src/pubSub/subscribe.js
@@ -1,18 +1,3 @@
-const path = require('path');
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-const PROTO_PATH = path.join(__dirname, '../protos/pubsub_api.proto');
-
-const packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true,
-    });
-const pubSubProto = grpc.loadPackageDefinition(packageDefinition).eventbus.v1;
-
 const subscribe = (client) => {
   return new Promise((resolve, reject) => {
     const call = client.subscribe();
