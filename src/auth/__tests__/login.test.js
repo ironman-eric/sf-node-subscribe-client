4a77fd19fa38b9c3ec47f548e9861428a09762a4
ironman-eric <eonno123@yahoo.com>
2023-09-17 17:54:39 -0700

    basic login

--- src/auth/__tests__/login.test.js
+++ src/auth/__tests__/login.test.js
@@ -1,14 +1,17 @@
+const superagent = require('superagent');
 const login = require('../login.js');
 
+jest.mock('superagent');
+
 describe('login', () => {
-  it('should login with username and password', () => {
+  it('should login with username and password', async () => {
     // ARRANGE
     const assert = "token";
     const userName = "user";
     const password = "password";
 
     // ACT
-    const result = login(userName, password);
+    const result = await login(userName, password);
 
     // ASSERT
     expect(result).toEqual(assert);
--- src/auth/login.js
+++ src/auth/login.js
@@ -1,5 +1,21 @@
-const login = (userName, password) => {  
-  return "token"
+const superagent = require('superagent');
+
+const login = async (userName, password) => {
+
+  const url = 'url';
+  
+  const data = `<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' " + \
+        "xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " + \
+        "xmlns:urn='urn:partner.soap.sforce.com'><soapenv:Body>" + \
+        "<urn:login><urn:username><![CDATA["${userName}
+        "]]></urn:username><urn:password><![CDATA["${password}
+        "]]></urn:password></urn:login></soapenv:Body></soapenv:Envelope>`;
+    
+  return await superagent
+    .post(url)
+    .send(data)
+    .set('content-type', 'text/xml')
+    .set('SOAPAction', 'Login')      
 };
 
 module.exports = login;
